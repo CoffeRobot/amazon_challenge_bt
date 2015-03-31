553ae12c33b321b94f2d7b0cfe392c84790dd38b
Michele Colledanchise <miccol@kth.se>
2015-03-31 18:16:29 +0200

    basic actions for PR2 added

---
+++ src/pr2_detect_server.cpp
@@ -0,0 +1,162 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib/client/simple_action_client.h>
+
+#include <bt_actions/BTAction.h>
+#include <pr2_controllers_msgs/PointHeadAction.h>
+typedef actionlib::SimpleActionClient<pr2_controllers_msgs::PointHeadAction> PointHeadClient;
+
+enum Status { RUNNING, SUCCESS, FAILURE};
+
+
+class BTAction
+{
+private:
+    PointHeadClient* point_head_client_;
+
+  ros::NodeHandle nh_;
+  // NodeHandle instance must be created before this line. Otherwise strange error may occur.
+  actionlib::SimpleActionServer<bt_actions::BTAction> as_; 
+  std::string action_name_;
+  // create messages that are used to published feedback/result
+  bt_actions::BTFeedback feedback_;
+  bt_actions::BTResult result_;
+
+public:
+
+  BTAction(std::string name) :
+    as_(nh_, name, boost::bind(&BTAction::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    as_.start();
+    point_head_client_ = new PointHeadClient("/head_traj_controller/point_head_action", true);
+
+    //wait for head controller action server to come up
+    while(!point_head_client_->waitForServer(ros::Duration(5.0))){
+      ROS_INFO("Waiting for the point_head_action server to come up");
+    }
+  }
+
+  ~BTAction(void)
+  {
+      delete point_head_client_;
+
+  }
+  //void executeCB()
+  void executeCB(const bt_actions::BTGoalConstPtr &goal)
+  {
+    // helper variables
+    ros::Rate r(1);
+    bool success = true;
+
+    // publish info to the console for the user
+    ROS_INFO("Starting Action");
+
+    // start executing the action
+    for(int i=1; i<=30; i++)
+    {
+
+     // check that preempt has not been requested by the client
+      if (as_.isPreemptRequested() || !ros::ok())
+      {
+        ROS_INFO("Action Halted");
+
+        // set the action state to preempted
+        as_.setPreempted();
+        point_head_client_->cancelGoal();
+        success = false;
+        break;
+      }
+      ROS_INFO("Executing Action");
+      //Looks at a point forward (x=5m), slightly left (y=1m), and 1.2m up
+      lookAt("base_link", 5.0, 1.0, 1.2);
+
+      //Looks at a point forward (x=5m), slightly right (y=-1m), and 1.2m up
+      lookAt("base_link", 5.0, -1.0, 1.2);
+
+
+      // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+      //r.sleep();
+    }
+
+    if(success)
+    {
+        setStatus(SUCCESS);
+    }
+  }
+
+
+  void lookAt(std::string frame_id, double x, double y, double z)
+  {
+    //the goal message we will be sending
+    pr2_controllers_msgs::PointHeadGoal goal;
+
+    //the target point, expressed in the requested frame
+    geometry_msgs::PointStamped point;
+    point.header.frame_id = frame_id;
+    point.point.x = x; point.point.y = y; point.point.z = z;
+    goal.target = point;
+
+    //we are pointing the high-def camera frame
+    //(pointing_axis defaults to X-axis)
+    goal.pointing_frame = "high_def_frame";
+
+    //take at least 0.5 seconds to get there
+    goal.min_duration = ros::Duration(0.5);
+
+    //and go no faster than 1 rad/s
+    goal.max_velocity = 1.0;
+
+    //send the goal
+    point_head_client_->sendGoal(goal);
+
+    //wait for it to get there (abort after 2 secs to prevent getting stuck)
+    point_head_client_->waitForResult(ros::Duration(2));
+  }
+
+
+
+
+
+//returns the status to the client (Behavior Tree)
+  void setStatus(int status){
+      //Set The feedback and result of BT.action
+      feedback_.status = status;
+      result_.status = feedback_.status;
+      // publish the feedback
+      as_.publishFeedback(feedback_);
+      // setSucceeded means that it has finished the action (it has returned SUCCESS or FAILURE).
+      as_.setSucceeded(result_);
+
+      switch(status){//Print for convenience
+      case SUCCESS:
+        ROS_INFO("Action %s Succeeded", ros::this_node::getName().c_str() );
+        break;
+      case FAILURE:
+          ROS_INFO("Action %s Failed", ros::this_node::getName().c_str() );
+        break;
+          ROS_ERROR("Action %s has a wrong return status", ros::this_node::getName().c_str() );
+      default:
+        break;
+      }
+  }
+
+
+
+
+
+
+};
+
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "action");
+      ROS_INFO(" Enum: %d",RUNNING);
+      ROS_INFO(" Action Ready for Ticks");
+  BTAction bt_action(ros::this_node::getName());
+  ros::spin();
+
+  return 0;
+}
---
+++ src/pr2_grasp_server.cpp
@@ -0,0 +1,207 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <pr2_controllers_msgs/JointTrajectoryAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <bt_actions/BTAction.h>
+typedef actionlib::SimpleActionClient< pr2_controllers_msgs::JointTrajectoryAction > TrajClient;
+
+
+enum Status { RUNNING, SUCCESS, FAILURE};
+
+
+class BTAction
+{
+protected:
+    TrajClient* traj_client_;
+  ros::NodeHandle nh_;
+  // NodeHandle instance must be created before this line. Otherwise strange error may occur.
+  actionlib::SimpleActionServer<bt_actions::BTAction> as_; 
+  std::string action_name_;
+  // create messages that are used to published feedback/result
+  bt_actions::BTFeedback feedback_;
+  bt_actions::BTResult result_;
+
+public:
+
+  BTAction(std::string name) :
+    as_(nh_, name, boost::bind(&BTAction::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    as_.start();
+    traj_client_ = new TrajClient("r_arm_controller/joint_trajectory_action", true);
+
+    // wait for action server to come up
+    while(!traj_client_->waitForServer(ros::Duration(5.0))){
+      ROS_INFO("Waiting for the joint_trajectory_action server");
+    }
+  }
+
+  ~BTAction(void)
+  {
+      delete traj_client_;
+
+  }
+  //void executeCB()
+  void executeCB(const bt_actions::BTGoalConstPtr &goal)
+  {
+    // helper variables
+    ros::Rate r(1);
+    bool success = true;
+
+    // publish info to the console for the user
+    ROS_INFO("Starting Action");
+
+    // start executing the action
+   // for(int i=1; i<=5; i++)
+    {
+
+
+        // Start the trajectory
+        startTrajectory(armExtensionTrajectory());
+        // Wait for trajectory completion
+        while(!getState().isDone() && ros::ok())
+        {
+          usleep(50000);
+        }
+     // check that preempt has not been requested by the client
+      if (as_.isPreemptRequested() || !ros::ok())
+      {
+        ROS_INFO("Action Halted");
+
+        // set the action state to preempted
+        as_.setPreempted();
+        success = false;
+        //break;//I cannot halt this. is just for demostantion purposes
+
+      }
+      ROS_INFO("Executing Action");
+
+
+      // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+      r.sleep();
+    }
+
+    if(success)
+    {
+        setStatus(SUCCESS);
+    }
+  }
+
+  //! Sends the command to start a given trajectory
+  void startTrajectory(pr2_controllers_msgs::JointTrajectoryGoal goal)
+  {
+    // When to start the trajectory: 1s from now
+    goal.trajectory.header.stamp = ros::Time::now() + ros::Duration(1.0);
+    traj_client_->sendGoal(goal);
+  }
+
+  //! Generates a simple trajectory with two waypoints, used as an example
+  /*! Note that this trajectory contains two waypoints, joined together
+      as a single trajectory. Alternatively, each of these waypoints could
+      be in its own trajectory - a trajectory can have one or more waypoints
+      depending on the desired application.
+  */
+  pr2_controllers_msgs::JointTrajectoryGoal armExtensionTrajectory()
+  {
+    //our goal variable
+    pr2_controllers_msgs::JointTrajectoryGoal goal;
+
+    // First, the joint names, which apply to all waypoints
+    goal.trajectory.joint_names.push_back("r_shoulder_pan_joint");
+    goal.trajectory.joint_names.push_back("r_shoulder_lift_joint");
+    goal.trajectory.joint_names.push_back("r_upper_arm_roll_joint");
+    goal.trajectory.joint_names.push_back("r_elbow_flex_joint");
+    goal.trajectory.joint_names.push_back("r_forearm_roll_joint");
+    goal.trajectory.joint_names.push_back("r_wrist_flex_joint");
+    goal.trajectory.joint_names.push_back("r_wrist_roll_joint");
+
+    // We will have two waypoints in this goal trajectory
+    goal.trajectory.points.resize(2);
+
+    // First trajectory point
+    // Positions
+    int ind = 0;
+    goal.trajectory.points[ind].positions.resize(7);
+    goal.trajectory.points[ind].positions[0] = 0.0;
+    goal.trajectory.points[ind].positions[1] = 0.0;
+    goal.trajectory.points[ind].positions[2] = 0.0;
+    goal.trajectory.points[ind].positions[3] = 0.0;
+    goal.trajectory.points[ind].positions[4] = 0.0;
+    goal.trajectory.points[ind].positions[5] = 0.0;
+    goal.trajectory.points[ind].positions[6] = 0.0;
+    // Velocities
+    goal.trajectory.points[ind].velocities.resize(7);
+    for (size_t j = 0; j < 7; ++j)
+    {
+      goal.trajectory.points[ind].velocities[j] = 0.0;
+    }
+    // To be reached 1 second after starting along the trajectory
+    goal.trajectory.points[ind].time_from_start = ros::Duration(1.0);
+
+    // Second trajectory point
+    // Positions
+    ind += 1;
+    goal.trajectory.points[ind].positions.resize(7);
+    goal.trajectory.points[ind].positions[0] = -0.3;
+    goal.trajectory.points[ind].positions[1] = 0.2;
+    goal.trajectory.points[ind].positions[2] = -0.1;
+    goal.trajectory.points[ind].positions[3] = -1.2;
+    goal.trajectory.points[ind].positions[4] = 1.5;
+    goal.trajectory.points[ind].positions[5] = -0.3;
+    goal.trajectory.points[ind].positions[6] = 0.5;
+    // Velocities
+    goal.trajectory.points[ind].velocities.resize(7);
+    for (size_t j = 0; j < 7; ++j)
+    {
+      goal.trajectory.points[ind].velocities[j] = 0.0;
+    }
+    // To be reached 2 seconds after starting along the trajectory
+    goal.trajectory.points[ind].time_from_start = ros::Duration(2.0);
+
+    //we are done; return the goal
+    return goal;
+  }
+
+  //! Returns the current state of the action
+  actionlib::SimpleClientGoalState getState()
+  {
+    return traj_client_->getState();
+  }
+
+
+//returns the status to the client (Behavior Tree)
+  void setStatus(int status){
+      //Set The feedback and result of BT.action
+      feedback_.status = status;
+      result_.status = feedback_.status;
+      // publish the feedback
+      as_.publishFeedback(feedback_);
+      // setSucceeded means that it has finished the action (it has returned SUCCESS or FAILURE).
+      as_.setSucceeded(result_);
+
+      switch(status){//Print for convenience
+      case SUCCESS:
+        ROS_INFO("Action %s Succeeded", ros::this_node::getName().c_str() );
+        break;
+      case FAILURE:
+          ROS_INFO("Action %s Failed", ros::this_node::getName().c_str() );
+        break;
+          ROS_ERROR("Action %s has a wrong return status", ros::this_node::getName().c_str() );
+      default:
+        break;
+      }
+  }
+};
+
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "action");
+      ROS_INFO(" Enum: %d",RUNNING);
+      ROS_INFO(" Action Ready for Ticks");
+  BTAction bt_action(ros::this_node::getName());
+  ros::spin();
+
+  return 0;
+}
